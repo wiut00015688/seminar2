19d914884bd9a14469c6d0123c4d7332ff18faf6
Azamat Tuxtayev <tukhtayevazamat7@gmail.com>
2023-10-28 20:10:37 +0500

    modified1

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("hello")
+#def myfunction():
+#    print("Hello World")
+
+#number = input("enter a random number")
+#if int(number) % 2 == 0:
+#    print("This is an even number")
+#else:
+#    print("This is an odd number")
+#    myfunction()
+
+
+def add(a, b):
+    return a+b
+print(add(6, 12))
+
