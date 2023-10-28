196b858e2b4ce2e0bb5b82cc6378373243c70403
Azamat Tuxtayev <tukhtayevazamat7@gmail.com>
2023-10-28 20:10:22 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+name = 'Azamat'
+module = 'CSF'
+print(f"Hello, My name is {name} and I'm studying {module}")
+
+#year = input("When did you enter WIUT?")
+#year_now = 2023
+#total = year_now - int(year)
+#print(f"You entered WIUT {total} years ago!")
+
+#cw_mark = input("Enter your CW mark")
+#exam_mark = input("Enter your exam mark")
+#total = (int(cw_mark)*0.4)+(int(exam_mark)*0.6)
+#print(f"Your total mark is - {total}")
+
+time_inminutes = 125
+time_inhour = time_inminutes // 60
+time_remainder = time_inminutes - time_inhour*60
+print(f"Movie is {time_inhour} h {time_remainder} mins")
+
+
+
+
+
+
+
